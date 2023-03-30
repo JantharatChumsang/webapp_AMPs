e7a82b3bec6fc6ae79ebfcdb8e6e3390efd471d7
Jantharat Chumsang <jantharatchjune@gmail.com>
2023-03-31 05:14:48 +0700

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -14,7 +14,6 @@ import numpy as np
 #Only need to set these here as we are add controls outside of Hydralit, to customise a run Hydralit!
 st.set_page_config(page_title='antimicrobial peptide',page_icon=":pill:",layout='wide',initial_sidebar_state='auto',)
 
-
 if __name__ == '__main__':
     with open('style2.css') as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
@@ -28,8 +27,8 @@ if __name__ == '__main__':
             favicon=":pill:",
             hide_streamlit_markers=hide_st,
             #add a nice banner, this banner has been defined as 5 sections with spacing defined by the banner_spacing array below.
-            use_banner_images=["resources/pig0.png",None,{'header':"<h1 style='text-align:center;padding: 0px 0px;color:F36C23;font-size:150%;'>WAAPP: Web Application for Antimicrobial Peptide Prediction</h1>"},None,"./resources/lock.png"], 
-            banner_spacing=[6.5,20,80,20,5],
+            use_banner_images=["resources/pig0.png",None,{'header':"<h1 style='text-align:center;padding: 0px 0px;color:F36C23;font-size:175%;'>WAAPP: Web Application for Antimicrobial Peptide Prediction</h1>"},None,"./resources/lock.png"], 
+            banner_spacing=[6,15,60,15,4.5],
             navbar_theme=over_theme
         )
 
@@ -40,7 +39,7 @@ if __name__ == '__main__':
 
         #add all application classes
         app.add_app("Predict your peptide", icon="🔍", app=apps.PredictApp(title="Predict your peptide"))
-        app.add_app("How to use", icon="❓", app=apps.HowtoApp(title="How to use"))
+        app.add_app("How to use web application", icon="❓", app=apps.HowtoApp(title="How to use web application"))
         app.add_app("Dashboard", icon="far fa-chart-bar", app=apps.DashbApp(title="Dashboard"))
         app.add_app("Intro", icon="🏆", app=apps.IntroApp(title="About us"))
         app.add_app("Member", icon="👩‍🏫", app=apps.MemberApp(title="Member"))
@@ -60,7 +59,7 @@ if __name__ == '__main__':
             complex_nav = {
                 'Home': ['Home'],
                 'Predict your peptide': ["Predict your peptide"],
-                'How to WebApp': ["How to use"],
+                'How to WebApp': ["How to use web application"],
                 'Dashboard': ['Dashboard'],
                 '🕮 About us': ['Intro',"Member"],
                 'Contact us': ['Contact us']
